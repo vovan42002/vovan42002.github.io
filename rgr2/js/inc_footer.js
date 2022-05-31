bbd8c34fb08ab99d13835701e58b2e0c2b47a258
vovan42002 <91207798+vovan42002@users.noreply.github.com>
2022-05-31 11:10:29 +0300

    Delete rgr2 directory

--- rgr2/js/inc_footer.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-    url: "footer.html",
-    dataType: "html",
-    success: function (response) {
-        document.getElementById('footer').innerHTML = response;
-    }
-});
--- rgr2/php/writeToFile.php
+++
@@ -1,7 +0,0 @@
-<?php
-    error_reporting(E_ALL);
-    $data = $_POST['text']; // the key we sent was "something"
-    $f = fopen("../files/secrets.txt", 'a+');
-    fwrite($f,"\n".$data);
-    fclose($f);
-?>
